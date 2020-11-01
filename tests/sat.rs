35274c4a42b700ae17f7e2b0c0cdd5c22997bc04
novacrazy <novacrazy@gmail.com>
2020-11-01 08:47:17 -0600

    Test edge cases

--- tests/sat.rs
+++ tests/sat.rs
@@ -2,34 +2,30 @@ use thermite::*;
 
 type Vi32 = <backends::AVX2 as Simd>::Vi32;
 
-#[test]
-fn test_sat_add_overflowing() {
-    let a = i32::MAX - 20;
-    let b = 210;
-    let cv = Vi32::splat(a).saturating_add(Vi32::splat(b));
-    assert_eq!(cv.extract(0), a.saturating_add(b));
+macro_rules! test_sat {
+    (SUB $name:ident => $a:expr; $b:expr) => {
+        #[test]
+        fn $name() {
+            let a = $a;
+            let b = $b;
+            let cv = Vi32::splat(a).saturating_sub(Vi32::splat(b));
+            assert_eq!(cv.extract(0), a.saturating_sub(b));
+        }
+    };
+    (ADD $name:ident => $a:expr; $b:expr) => {
+        #[test]
+        fn $name() {
+            let a = $a;
+            let b = $b;
+            let cv = Vi32::splat(a).saturating_add(Vi32::splat(b));
+            assert_eq!(cv.extract(0), a.saturating_add(b));
+        }
+    };
 }
 
-#[test]
-fn test_sat_sub_overflowing() {
-    let a = i32::MIN + 20;
-    let b = 210;
-    let cv = Vi32::splat(a).saturating_sub(Vi32::splat(b));
-    assert_eq!(cv.extract(0), a.saturating_sub(b));
-}
-
-#[test]
-fn test_sat_add_regular() {
-    let a = i32::MAX - 20;
-    let b = 2;
-    let cv = Vi32::splat(a).saturating_add(Vi32::splat(b));
-    assert_eq!(cv.extract(0), a.saturating_add(b));
-}
-
-#[test]
-fn test_sat_sub_regular() {
-    let a = i32::MIN + 20;
-    let b = 2;
-    let cv = Vi32::splat(a).saturating_sub(Vi32::splat(b));
-    assert_eq!(cv.extract(0), a.saturating_sub(b));
-}
+test_sat!(ADD test_sat_add_overflowing => i32::MAX - 20; 210);
+test_sat!(SUB test_sat_sub_overflowing => i32::MIN + 20; 210);
+test_sat!(ADD test_sat_add_regular => i32::MAX - 20; 2);
+test_sat!(SUB test_sat_sub_regular => i32::MIN + 20; 2);
+test_sat!(ADD test_sat_add_regular_alt => -1; 2);
+test_sat!(SUB test_sat_sub_regular_alt =>  1; 2);
