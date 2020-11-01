9d7c5142d390c1439350271a33e014031dc63d86
novacrazy <novacrazy@users.noreply.github.com>
2020-11-01 03:56:09 +0000

    deploy: 0f0cdea8320865d9f9903c610bdb7beaad3805b8

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["thermite"] = [{"text":"impl Eq for AVX2","synthetic":false,"types":[]},{"text":"impl Eq for i32x8&lt;AVX2&gt;","synthetic":false,"types":[]},{"text":"impl Eq for u64x8&lt;AVX2&gt;","synthetic":false,"types":[]}];
+implementors["thermite"] = [{"text":"impl Eq for AVX2","synthetic":false,"types":[]},{"text":"impl Eq for i32x8&lt;AVX2&gt;","synthetic":false,"types":[]},{"text":"impl Eq for u32x8&lt;AVX2&gt;","synthetic":false,"types":[]},{"text":"impl Eq for u64x8&lt;AVX2&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
