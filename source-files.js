39de73eae02728b1e9bb9f1a3be6f508405d3776
novacrazy <novacrazy@users.noreply.github.com>
2020-11-05 15:35:56 +0000

    deploy: a0fb36888a6571f5001506cce87242ffc78e01ed

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["thermite"] = {"name":"","dirs":[{"name":"backends","dirs":[{"name":"avx2","files":["mod.rs","vf32.rs","vf64.rs","vi32.rs","vu32.rs","vu64.rs"]}],"files":["macros.rs","mod.rs"]}],"files":["buffer.rs","lib.rs","macros.rs","mask.rs","math.rs","pointer.rs"]};
+sourcesIndex["thermite"] = {"name":"","dirs":[{"name":"backends","dirs":[{"name":"avx2","files":["mod.rs","vf32.rs","vf64.rs","vi16.rs","vi32.rs","vu32.rs","vu64.rs"]}],"files":["macros.rs","mod.rs"]},{"name":"math","files":["common.rs","mod.rs","vf32.rs","vf64.rs"]}],"files":["buffer.rs","lib.rs","macros.rs","mask.rs","pointer.rs"]};
 createSourceSidebar();
