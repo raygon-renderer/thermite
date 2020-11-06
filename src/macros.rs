c30d0ad0c00ca79ff74167c7e7c4a3fe90f4aa3b
novacrazy <novacrazy@gmail.com>
2020-11-05 22:31:31 -0600

    Impl (un)likely on stable

--- src/macros.rs
+++ src/macros.rs
@@ -1,35 +1,38 @@
-#![allow(unused)]
-
 #[cfg(feature = "nightly")]
-macro_rules! likely {
-    ($e:expr) => {{
-        #[allow(unused_unsafe)]
-        unsafe {
-            std::intrinsics::likely($e)
-        }
-    }};
-}
+pub use std::intrinsics::{likely, unlikely};
 
-#[cfg(feature = "nightly")]
-macro_rules! unlikely {
-    ($e:expr) => {{
-        #[allow(unused_unsafe)]
-        unsafe {
-            std::intrinsics::unlikely($e)
-        }
-    }};
+// borrows technique from https://github.com/rust-lang/hashbrown/pull/209
+#[cfg(not(feature = "nightly"))]
+#[inline]
+#[cold]
+fn cold() {}
+
+#[cfg(not(feature = "nightly"))]
+#[rustfmt::skip]
+#[inline(always)]
+pub unsafe fn likely(b: bool) -> bool {
+    if !b { cold() } b
 }
 
 #[cfg(not(feature = "nightly"))]
-macro_rules! likely {
-    ($e:expr) => {
-        $e
-    };
+#[rustfmt::skip]
+#[inline(always)]
+pub unsafe fn unlikely(b: bool) -> bool {
+    if b { cold() } b
 }
 
-#[cfg(not(feature = "nightly"))]
+#[rustfmt::skip]
+macro_rules! likely {
+    ($e:expr) => {{
+        #[allow(unused_unsafe)]
+        unsafe { $crate::macros::likely($e) }
+    }};
+}
+
+#[rustfmt::skip]
 macro_rules! unlikely {
-    ($e:expr) => {
-        $e
-    };
+    ($e:expr) => {{
+        #[allow(unused_unsafe)]
+        unsafe { $crate::macros::unlikely($e) }
+    }};
 }
