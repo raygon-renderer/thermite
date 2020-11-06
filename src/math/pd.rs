05ad2971e31ea9a6f33beaceca2121941dbd83d3
novacrazy <novacrazy@gmail.com>
2020-11-06 00:38:56 -0600

    pd erf

--- src/math/pd.rs
+++ src/math/pd.rs
@@ -65,9 +65,43 @@ where
     fn log10(x: Self::Vf) -> Self::Vf {
         unimplemented!()
     }
+
+    #[rustfmt::skip]
+    #[inline(always)]
     fn erf(x: Self::Vf) -> Self::Vf {
-        unimplemented!()
+        let a0 = Vf64::<S>::one();
+        let a1 = Vf64::<S>::splat(0.141047395888);
+        let a2 = Vf64::<S>::splat(0.0895246554342);
+        let a3 = Vf64::<S>::splat(0.024538446357);
+        let a4 = Vf64::<S>::splat(0.00339526031482);
+        let a5 = Vf64::<S>::splat(0.00127101693092);
+        let a6 = Vf64::<S>::splat(0.000343596421733);
+        let a7 = Vf64::<S>::splat(-0.0000282694821623);
+        let a8 = Vf64::<S>::splat(0.0000153312079619);
+        let a9 = Vf64::<S>::splat(0.00000806034527525);
+        let a10 = Vf64::<S>::splat(-0.00000491119825703);
+        let a11 = Vf64::<S>::splat(0.00000190850200269);
+        let a12 = Vf64::<S>::splat(-4.5433487004e-7);
+        let a13 = Vf64::<S>::splat(7.5111413853e-8);
+        let a14 = Vf64::<S>::splat(-7.4944859806e-9);
+        let a15 = Vf64::<S>::splat(3.8381832932e-10);
+
+        let b = a0 - (a0 - x.abs()); // crush denormals
+        let b2 = b * b;
+        let b4 = b2 * b2;
+
+        let r = poly_15(
+            b, b2, b4, b4 * b4,
+            a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15,
+        );
+
+        let r2 = r * r;
+        let r4 = r2 * r2;
+        let r8 = r4 * r4;
+
+        (a0 - a0 / r8).copysign(x)
     }
+
     fn erfinv(x: Self::Vf) -> Self::Vf {
         unimplemented!()
     }
