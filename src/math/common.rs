1c2164e35f863a4362a62dd5c1cd3ed3461eb9e8
novacrazy <novacrazy@gmail.com>
2020-11-08 10:43:33 -0600

    Fix poly5

--- src/math/common.rs
+++ src/math/common.rs
@@ -20,7 +20,7 @@ pub fn poly_4<S: Simd, V: SimdFloatVector<S>>(x: V, x2: V, x4: V, c0: V, c1: V, 
 #[inline(always)]
 pub fn poly_5<S: Simd, V: SimdFloatVector<S>>(x: V, x2: V, x4: V, c0: V, c1: V, c2: V, c3: V, c4: V, c5: V) -> V {
     // x^4 * (x * c5 + c4) + (x^2 * (x * c3 + c2) + (x*c1 + c0))
-    x4.mul_add(x.mul_add(c4, c4), x2.mul_add(x.mul_add(c3, c2), x.mul_add(c1, c0)))
+    x4.mul_add(x.mul_add(c5, c4), x2.mul_add(x.mul_add(c3, c2), x.mul_add(c1, c0)))
 }
 
 #[rustfmt::skip]
