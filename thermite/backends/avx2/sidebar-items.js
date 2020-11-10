d9ffdcc49925af4296b0cbde32e4e52e66d2bf04
novacrazy <novacrazy@users.noreply.github.com>
2020-11-10 19:49:22 +0000

    deploy: 62c9cf3370a4c7a97c2387185cc1cba3038f2bca

--- thermite/backends/avx2/sidebar-items.js
+++ thermite/backends/avx2/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AVX2",""],["f32x8",""],["f64x8",""],["i16x8",""],["i32x8",""],["i64x8",""],["u32x8",""],["u64x8",""]]});
+initSidebarItems({"struct":[["AVX2",""],["i16x8",""]]});
