3248e387536307ad8303ca59df07f4ef5c403dd5
novacrazy <novacrazy@users.noreply.github.com>
2020-11-25 02:17:02 +0000

    deploy: 43d20824772d5156d957c2d6468656bdfd311050

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["thermite"] = [{"text":"impl&lt;S:&nbsp;Simd, V:&nbsp;SimdVectorBase&lt;S&gt;&gt; Deref for SimdBuffer&lt;S, V&gt;","synthetic":false,"types":[]}];
+implementors["thermite"] = [{"text":"impl&lt;S:&nbsp;Simd, V:&nbsp;SimdVectorBase&lt;S&gt;&gt; Deref for SimdBuffer&lt;S, V&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; Deref for BranchfreeDivider&lt;T&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
