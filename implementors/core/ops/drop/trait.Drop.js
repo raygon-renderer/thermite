76ea9e0baac1c12bedaeecad522aac09254d6ace
novacrazy <novacrazy@users.noreply.github.com>
2021-03-25 15:59:47 +0000

    deploy: 48978c4be1630ebad601e27103c3baa5874e3954

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["thermite"] = [{"text":"impl&lt;S:&nbsp;Simd, V:&nbsp;SimdVectorBase&lt;S&gt;&gt; Drop for VectorBuffer&lt;S, V&gt;","synthetic":false,"types":[]}];
+implementors["thermite"] = [{"text":"impl&lt;S:&nbsp;<a class=\"trait\" href=\"thermite/trait.Simd.html\" title=\"trait thermite::Simd\">Simd</a>, V:&nbsp;<a class=\"trait\" href=\"thermite/trait.SimdVectorBase.html\" title=\"trait thermite::SimdVectorBase\">SimdVectorBase</a>&lt;S&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"thermite/struct.VectorBuffer.html\" title=\"struct thermite::VectorBuffer\">VectorBuffer</a>&lt;S, V&gt;","synthetic":false,"types":["thermite::buffer::VectorBuffer"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
