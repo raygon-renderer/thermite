0b5d10db9d2b92aa5507c0e9d50ea09055de5dc4
novacrazy <novacrazy@gmail.com>
2021-03-30 10:09:15 -0500

    Add `reverse` method to vectors

--- crates/thermite/src/lib.rs
+++ crates/thermite/src/lib.rs
@@ -280,6 +280,24 @@ pub trait SimdVectorBase<S: Simd + ?Sized>: 'static + Sized + Copy + Debug + Def
         res
     }
 
+    /// Reverses the elements in the vector
+    #[inline(always)]
+    #[rustfmt::skip]
+    fn reverse(self) -> Self {
+        // TODO: Expand this if necessary, but as of now no vectors are longer than 8 anyway
+        match Self::NUM_ELEMENTS {
+            0 | 1 => self,
+            2 => shuffle!(self, [1, 0]),
+            4 => shuffle!(self, [3, 2, 1, 0]),
+            8 => shuffle!(self, [7, 6, 5, 4, 3, 2, 1, 0]),
+            16 => shuffle!(self, [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
+            32 => shuffle!(
+                self, [31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+            ),
+            _ => unreachable!(),
+        }
+    }
+
     #[inline(always)]
     #[cfg(feature = "alloc")]
     fn alloc(count: usize) -> VectorBuffer<S, Self> {
